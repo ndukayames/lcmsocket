b0f6daf31046bdda5d9d170674aa4aeba18418d5
Obi James Onyemaechi <jamesobi@softprism.org>
2021-07-09 14:12:49 +0100

    first push

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ let io = require('socket.io')(server);
 io.on('connection',(socket) => {
   socket.emit('connection_status', 'socket is working')
    console.log("user connected")
+  socket.on('test_socket_connection',(arg) => {
+    console.log(arg)
+  })
 })
 var port = process.env.PORT || 3001;
 server.listen(port, function(){
    console.log('listening in http://localhost:' + port);
 });
-
-//  setInterval(() => {
-//   socket.broadcast.emit('check-something',{message:'something is here'})
-// }, 100);
