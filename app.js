b09df6fe7141503cb3f5591f7085372bdd5fbc59
Obi James Onyemaechi <jamesobi@softprism.org>
2021-07-19 04:29:43 +0100

    working

--- app.js
+++ app.js
@@ -7,10 +7,41 @@ io.on('connection', (socket) => {
   socket.on('disconnect', function(){
     console.log('user disconnected')
   });
+  socket.on('create_my_class', (class_param) => {
+    // activates when a new class has been created by the HOC
+    // then emits a response with the class parameter
+    // which is used by the app to check for the students to notify of the new class
+    socket.broadcast.emit('check_new_class', class_param)
+  })
  socket.on('test_connection', function() {
+   // used to test the socket's connection
    console.log('test received')
    io.emit('test_result', {result: 'test successful'})
  })  
+ socket.on('change_class_status', (classParam) => {
+   // activates when the status of a class changes
+   console.log(classParam)
+  io.emit('class_status_changed', classParam)
+ })
+ socket.on('create_new_assignment', (assignmentParam) => {
+  // activates when the lecturer creates a new assignment
+  // a response is sent to the students who registered the courses
+  socket.broadcast.emit('new_assignment_created', assignmentParam)
+ })
+ socket.on('edit_assignment', (assignmentParam) => {
+  // activates when the lecturer creates a new assignment
+  // a response is sent to the students who registered the courses
+  socket.broadcast.emit('assignment_edited', assignmentParam)
+ })
+ socket.on('submit_student_score', (scoreParam) => {
+   // activates when lecturer submits students score
+   // a response is sent to the student who has been scored
+   socket.broadcast.emit('new_assignment_score', scoreParam)
+ })
+ socket.on('student_submit_assignment', (course_id) => {
+   //activates when a student uploads a document to submit their assignment
+   socket.broadcast.emit('new_assignment_submission', course_id)
+ })
 });
  
 var port = process.env.PORT || 3001;
